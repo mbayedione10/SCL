b639afc3ac40cd504a15411db1b95f6318474c49
mbayedione10 <mbayedione10@gmail.com>
2021-05-07 12:41:57 +0000

    add delete option and prompt password without echoing

--- Mails/markRead.py
+++ Mails/markRead.py
@@ -2,35 +2,30 @@
 #https://stackoverflow.com/questions/51596061/find-the-subject-of-a-specific-email-imap-python
 #https://desktop.arcgis.com/fr/arcmap/latest/analyze/executing-tools/scheduling-a-python-script-to-run-at-prescribed-times.htm
 import  imaplib
-
+import datetime
 import getpass
 
-#connexion
-
+date = (datetime.date.today() - datetime.timedelta(days=15)).strftime("%d-%b-%Y")
 mail = imaplib.IMAP4_SSL('Outlook.office365.com')
 
-print("Entrer votre login svp:")
-log = input()
-print("Entrer votre mdp svp:")
-mdp = input()
+log = input("Entrer votre login svp: ")
+mdp = getpass.getpass()
 
 mail.login(log,mdp)
 mail.select()
 
-#search specific mails
-typ, data = mail.search(None,'FROM', "f.sow@sesam.sn", "UNSEEN", 'SUBJECT', "Success")
-#typ, data = mail.search(None,'FROM', "f.sow@sesam.sn", "SEEN", 'SUBJECT', "Success")
+#Search mailbox for matching messages.
+#'mark' and 'delete' are space separated list of matching message numbers.
+typ1, mark = mail.search(None,'FROM', "f.sow@sesam.sn", "UNSEEN", 'SUBJECT', "Success")
+typ2, delete = mail.search(None,'FROM', "f.sow@sesam.sn", "SEEN", 'SUBJECT', "Success","SENTBEFORE", date)
 
 
-#Lire
-for num in data[0].split():
-    #Read
-    typ, data = mail.fetch(num, '(RFC822)')
-    #Delete
-    #mail.store(num, '+FLAGS', '(\\Deleted)')
+for num in mark[0].split():py
+    typ1, mark = mail.fetch(num, '(RFC822)')
 
-#print("deleted succesfully")
-
+for sup in delete[0].split():
+    mail.store(sup, '+FLAGS', '(\\Deleted)')
+print("Mark and delete done")
 
 mail.close()  #close the mail box
 mail.logout() #logout
